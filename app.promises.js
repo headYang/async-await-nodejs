683b8f42f658159760094b65652b1393129e74da
Jin Yang <yang.youth1020@gmail.com>
2018-06-30 11:52:58 +0800

    Add promises

--- app.promises.js
+++ app.promises.js
@@ -11,7 +11,19 @@ const users = [
     }
 ];
 
-const grades = [];
+const grades = [{
+    id: 1,
+    schoolId: 101,
+    grade: 86
+}, {
+    id: 2,
+    schoolId: 999,
+    grade: 190
+}, {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+}];
 
 const getUser = (id) => {
     return new Promise((resolve, reject) => {
@@ -24,6 +36,38 @@ const getUser = (id) => {
     });
 };
 
+const getGrades = (schoolId) => {
+    return new Promise((resolve, reject) => {
+        resolve(grades.filter((grade) => grade.schoolId === schoolId));
+    })
+};
+
+const getStatus = (userId) => {
+    let user;
+    return getUser(userId).then((tempUser) => {
+        user = tempUser;
+        return getGrades(user.schoolId);
+    }).then((grades) => {
+        let average = 0;
+        if(grades.length > 0) {
+            average = grades.map((grade) => grade.grade).reduce((a, b) => {
+                return a + b;
+            }) / grades.length;
+        }
+        console.log('average: ', average);
+        return `average is ${average}`;
+    })
+}
+
+getStatus(1).then((status) => {
+    console.log(status);
+}).catch((e) => {
+    console.log(e);
+});
 getUser(2).then((user) => {
     console.log(user);
 }).catch((e) => { console.log(e); });
+
+getGrades(102).then((grades) => {
+    console.log(grades);
+}).catch((err) => console.log(err));
